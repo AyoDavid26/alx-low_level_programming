787999f31257065672e85730d7590281adad6758
AyoDavid26 <folayanayodeji@gmail.comgit config --global user.email clear>
2022-10-16 15:13:44 -0700

    my low level programming commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-program entry point.
+ *
+ * Return: 0 if no error, non-zerovalue if error.
+ */
+int main(void)
+{
+		int n;
+		int ldigit;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		ldigit = n % 10;
+		if (1digit > 5)
+		{
+			printf("last digit of %d is %d and is greater than 5\n", n, 1digit);
+		}
+		else if (1digit == 0)
+		{
+			printf("last digit of %d is %d and is 0\n", n, 1digit);
+		}
+		else if (1digit < 6 && 1digit != 0)
+		{
+			printf("last digit of %d is %d and is less than 6 and not 0\n", n, 1digit);
+		}
+		return (0);
+}
