750ab3fdc24bc55bcfcba69ad8712a56597a2ec3
AyoDavid26 <folayanayodeji@gmail.comgit config --global user.email clear>
2022-10-18 17:01:17 -0700

    functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,30 +2,30 @@
 
 #define main_h
 
-extern int _putchar(char c);
+int _putchar(char c);
 
-extern void print_alphabet(void);
+void print_alphabet(void);
 
-extern void print_alphabet_x10(void);
+void print_alphabet_x10(void);
 
-extern int _islower(int c);
+int _islower(int c);
 
-extern int _isalpha(int c);
+int _isalpha(int c);
 
-extern int print_sign(int n);
+int print_sign(int n);
 
-extern int _abs(int);
+int _abs(int);
 
-extern int print_last_digit(int);
+int print_last_digit(int);
 
-extern void jack_bauer(void);
+void jack_bauer(void);
 
-extern void times_table(void);
+void times_table(void);
 
-extern int add(int, int);
+int add(int, int);
 
-extern void print_to_98(int n);
+void print_to_98(int n);
 
-extern void print_times_table(int n);
+void print_times_table(int n);
 
 #endif
