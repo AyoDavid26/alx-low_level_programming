34cda7a254fc5dc158367982782403ad69334942
AyoDavid26 <folayanayodeji@gmail.comgit config --global user.email clear>
2022-10-20 14:13:38 -0700

    Debugging

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first int
+ * @b: second int
+ * @c: third int
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest number = b;
+	if (a > b)
+	{
+		largest = a;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+	return (largest);
+}
