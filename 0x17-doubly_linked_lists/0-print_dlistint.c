bfad8d6b7a5b790b8d65e0022ca08f48fc8e717a
AyoDavid26 <folayanayodeji@gmail.com>
2024-01-25 08:30:55 -0800

    DouBlY LinKEd Lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -9,16 +10,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
-	count = 0;
-	if(h == NULL)
-		return (count);
+	size_t node_count =0;
 	
 	while (h!= NULL)
 	{
 		printf("%d/n", h->n);
-		count++;
 		h = h->next;
+		node_count++;
 	}
-	return (count);
+	return (node_count);
 }
