8c1d5b0a17aaeace8ce84f32f4afd434828eef11
AyoDavid26 <folayanayodeji@gmail.com>
2024-05-22 17:11:38 -0700

    SeaRcH AlGoRiThm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "serach_algos.h"
+
+/**
+ * Linear_Search - To search for a value in an array using
+ * linear search algorithm
+ *
+ * @array: A pointer to the first element of the arrayto search
+ * @size: Number of elements in the array
+ * @value: Value to search
+ *
+ * Return: the first index where the value is located
+ */
+int linear_search(int *array, size_t size, int value);
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
